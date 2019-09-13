2ef685e0d3f025f57165a11e70bcda7cbaedcb09
islaterm <ignacio.slater@ug.uchile.cl>
2019-09-13 16:48:40 -0300

    bot package skeleton v0.1b2

---
+++ bot/__init__.py
@@ -0,0 +1,6 @@
+"""
+Definitions for the bot module
+"""
+__author__ = "Ignacio Slater Muñoz <islaterm@gmail.com>"
+__version__ = "0.1b2"
+__all__ = ["animols"]
--- bot/animols.py
+++ bot/animols.py
@@ -1,3 +1,26 @@
 """
-
+Animols Gifter bot's manager
 """
+from telegram.ext import Updater
+
+
+class Gifter:
+    """
+    Base class for @animolsgifbot.
+    """
+    _updater: Updater
+
+    def __init__(self, token: str):
+        """
+        Initializes the bot.
+
+        :param token:
+            Bot's token
+        """
+        self._updater = Updater(token)
+
+    def run(self) -> None:
+        """
+        Runs the bot.
+        """
+        self._updater.start_polling()
